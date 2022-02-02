22849d4f00e02a522dddd5f19c6587f17104f735
SamuelLopezF <Samuellopez.f@gmail.com>
2022-02-02 10:33:17 -0500

    a1 first push

---
+++ src/WarehouseManager.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WarehouseManager {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ALDictionary<String, String> array_based_manager = new ALDictionary<>(100);
+        try {
+            BufferedReader buff_reader = new BufferedReader(new FileReader("src/warehouse_data"));
+            String line;
+            while((line = buff_reader.readLine()) != null)
+            {
+                String key = line.substring(0,6);
+                String info = line.substring(7,14);
+                array_based_manager.insert(key, info);
+            }
+            }catch (IOException e)
+            {
+                System.out.println("could not read file");
+            }
+        System.out.println(array_based_manager.search("IN0012"));
+        array_based_manager.toString();
+    }
+}
